7702e948a3ef7d0cece38ce0a6aa871b5eb7cd36
Ramir Mesquita <ramir.mesquita@gmail.com>
2019-08-13 19:57:40 -0300

    First API endpoints using Express. Dummy only.

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+
+var app = express();
+
+app.use(bodyParser.urlencoded( {extended:true }));
+app.use(bodyParser.json());
+
+var port = 8080;
+
+app.listen(port);
+
+var db = new mongodb.Db(
+    'locations',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
+
+console.log('HTTP Server listening to port %s', port);
+
+app.get('/', function(req, res) {
+
+    res.send({message:'First Location'});
+});
+
+app.post('/api', function(req, res) {
+    
+    var data = req.body;
+    res.send(data);
+})
